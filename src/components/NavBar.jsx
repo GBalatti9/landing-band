b8a236fbd2dc4afced0aafa967781cc74ebbfa79
GBalatti9 <gas.balatti@gmail.com>
2023-12-20 11:18:53 -0300

    Menu behaviour

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,32 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './NavBar.css';
-import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 export const NavBar = () => {
-
     const [open, setOpen] = useState(false);
 
     const handleMenuMobile = () => {
-        setOpen(!open)
-    }
+        setOpen(!open);
+    };
 
     return (
         <>
-        <div>
             <div className='bg-white'>
-                {
-                    open 
-                    ? <FontAwesomeIcon icon={faXmark} onClick={ handleMenuMobile } className='button-menu'/>
-                    : <FontAwesomeIcon icon={faBars} onClick={ handleMenuMobile } className='button-menu'/>
-                }
+                <FontAwesomeIcon
+                    icon={open ? faTimes : faBars}
+                    onClick={handleMenuMobile}
+                    className='button-menu'
+                />
             </div>
-            <div className={`${open ? 'menu-mobile-display' : 'menu-mobile'}`}>
-                
+            <div className={`menu-mobile ${open ? 'active' : ''}`}>
+                {/* Contenido del menú móvil */}
+                <ul className='list-menu'>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact</li>
+                </ul>
             </div>
-        </div>
-        <ul className='list-menu'>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Contact</li>
-        </ul>
         </>
-    )
-}
+    );
+};
